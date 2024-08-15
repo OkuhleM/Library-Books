187749d595425ae6452f384c451ef780ab5721b7
OkuhleM <lindokuhlea38@gmail.com>
2024-08-15 12:53:11 +0200

    admin register page

--- client/src/Components/AdminLogin.js
+++ client/src/Components/AdminLogin.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react';
+import axios from 'axios';
+import { Navigate, useNavigate } from 'react-router';
 
 export default function AdminLogin() {
+
+const [values, setValues] = useState({
+    "email":"",
+    "password":""
+})
+const [saveValues, setSaveValues] = useState([])
+
+const navigate = useNavigate()
+
+
+const handleChange = (e) => {
+    e.preventDefault()
+    setValues({...values,[e.target.name]: e.target.value})
+}
+
+
+const handleLogin = (e) => {
+    e.preventDefault()
+axios.post("http://localhost:3000/admin-login",values)
+.then(res => {
+    console.log('res', res)
+    if(res.data.Status === "Success"){
+        console.log("Success")
+        return navigate('/admindashboard')
+    } else if(res.data.Status !== "Success") {
+        alert("Admin Not Found, please register!!")
+return navigate('/useradmin/adminregister')
+    } else{
+        console.log(res.data.err)
+    }
+})
+}
+
   return (
     <div>
       <h2>Log In</h2>
@@ -8,11 +43,11 @@ export default function AdminLogin() {
 
 <div className='admin-form'>
     <label>Email:</label>
-    <input type='email' /> <br/>
+    <input type='email' name='email' value={values.email} onChange={handleChange}/> <br/>
     <label>Password:
     </label>
-    <input type='password'/><br/>
-    <button>Log in</button>
+    <input type='password' name='password' value={values.password} onChange={handleChange}/><br/>
+    <button onClick={handleLogin}>Log in</button>
 </div>
 
     </div>
